5a43d6291f4bb3ab1c336e4df4197672a7963bb4
aysun013 <ismailovaaysuna28@gmail.com>
2024-09-08 01:20:39 +0400

    change/structure

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+let movies = [
+    {
+        img: "../image/Rectangle 108.png",
+        name: "The Dry 2"
+    },
+    {
+        img: "../image/Rectangle 106.png",
+        name: "Trigger Warning"
+    },
+    {
+        img: "../image/Rectangle 110.png",
+        name: "The Dry"
+    },
+    {
+        img: "../image/Rectangle 112.png",
+        name: "Clanes"
+    },
+    {
+        img: "../image/Rectangle 114.png",
+        name: "Interstellar"
+    },
+    {
+        img: "../image/Rectangle 116.png",
+        name: "The Beekeeper"
+    }
+]
+
+let writer = ""
+movies.forEach(function (m) {
+    writer += `
+    <div class="card">
+        <div class="inner">
+            <div class="image">
+                <img src="${m.img}" alt="">
+            </div>
+            <div class="name">${m.name}</div>
+        </div>
+    </div>
+  `
+});
+
+let div = document.querySelector(".cards");
+div.innerHTML= writer;
